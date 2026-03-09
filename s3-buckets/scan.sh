197dffc621b6462b06ed721b5bb8a804ca3e446a
Potassium Permanganate <99038567+ChemicalSalt@users.noreply.github.com>
2026-03-10 01:23:24 +0530

    Add shebang and comments to scan.sh

--- s3-buckets/scan.sh
+++ s3-buckets/scan.sh
@@ -1,1 +1,4 @@
-
+#!/bin/bash
+# Check for open/misconfigured S3 buckets
+subfinder -dL domains.txt -silent -o s3-buckets/subs.txt
+nuclei -l s3-buckets/subs.txt -t misconfiguration/aws -o s3-buckets/results.txt
