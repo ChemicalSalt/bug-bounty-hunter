962f78a8763ac969f34f1de9a4f29b90adff7125
Potassium Permanganate <99038567+ChemicalSalt@users.noreply.github.com>
2026-03-10 01:22:17 +0530

    Initialize subdomain takeover scan script
    Added a Bash shebang and commands for subdomain scanning.

--- subdomain-takeover/scan.sh
+++ subdomain-takeover/scan.sh
@@ -1,1 +1,4 @@
-
+#!/bin/bash
+subfinder -dL domains.txt -silent -o subdomain-takeover/subs.txt
+httpx -l subdomain-takeover/subs.txt -cname -status-code -mc 404 -o subdomain-takeover/cname_404.txt
+nuclei -l subdomain-takeover/subs.txt -t takeovers -o subdomain-takeover/results.txt
